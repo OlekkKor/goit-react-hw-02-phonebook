17efd832cf5f7cec464639357a41b1325aa2ba4c
unknown <olekkorovin@gmail.com>
2022-12-19 22:16:38 +0200

    Hm 2.2 ready

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,9 @@ import css from './Filter.module.css';
 
 export const Filter = ({onInputChange }) => { 
     return (
+        <>
+        <h5 className={css.filterHeader}>Find contacts by name</h5>
         <input className={css.filter} type='text' onChange={(e) => onInputChange(e.target.value)}/>
+        </>
     )
 }
